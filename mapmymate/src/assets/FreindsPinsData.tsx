c1584a00fd562af078ccee91d001d03a7ae65019
usmanashrf <45431758+usmanashrf@users.noreply.github.com>
2023-07-03 01:49:52 +0500

    add popups on marker

---
+++ mapmymate/src/assets/FreindsPinsData.tsx
@@ -0,0 +1,19 @@
+import profile from '/public/profile.png';
+
+let friendsPins =[{
+    Name:"Jhon",
+    Location:{Latitude:37.8, Longitude:-122.4},
+    profile:profile
+},
+{
+    Name:"Mike",
+    Location:{Latitude:37.6879, Longitude:-122.4702},
+    profile:profile
+},
+{
+    Name:"Luke",
+    Location:{Latitude:37.8044, Longitude:-122.2712},
+    profile:profile
+}]
+
+export default friendsPins
--- mapmymate/src/components/map/index.tsx
+++ mapmymate/src/components/map/index.tsx
@@ -1,25 +1,93 @@
 'use client';
-import Map, {Marker} from 'react-map-gl';
+import Map, {GeolocateControl, Marker, NavigationControl, Popup} from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
+import { useEffect, useState, useRef } from 'react';
+import friendsPins from "@/assets/FreindsPinsData";
+import { MdLocationOn } from "react-icons/md";
 
 export default function MateMap() {
-  return (
-    <div className='flex-1 h-full'>
-        <Map
-      mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_KEY}
-      initialViewState={{
-        longitude: -122.4,
-        latitude: 37.8,
-        zoom: 12
-      }}
-      mapStyle="mapbox://styles/mapbox/streets-v9"
-    >
-        <Marker longitude={-122.4} latitude={37.8} color='red' onClick={()=>(window.alert("longitude={-122.4} latitude={37.8}"))} >
-       </Marker>
+  let [showPopup, setShowPopup] = useState<boolean>();
+  const[freindspinData, setFreindspinData] = useState(friendsPins);
+	const [selectedMarker, setSelectedMarker] = useState(null);
+	const mapRef = useRef(null);
 
-       <Marker longitude={-122.5} latitude={37.8} color='blue' onClick={()=>(window.alert("longitude={-122.5} latitude={37.8}"))} >
-       </Marker>
-        </Map>
+
+// Friends Pin will be coming from backend API
+useEffect(()=>{
+  setFreindspinData(friendsPins)
+  console.log(freindspinData);
+},[])
+
+  const handleMarkerClick = (pin:any) => {
+    console.log("Before updating",showPopup);
+    console.log("Name of Friebd",pin?.Name);
+    setTimeout(()=>(setShowPopup(true)),10);
+    // setShowPopup(true);
+    console.log("After updating",showPopup);
+  }
+  return (
+    <div className="flex-1 h-full">
+      <Map
+        ref={mapRef}
+        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_KEY}
+        mapStyle="mapbox://styles/mapbox/streets-v12"
+        initialViewState={{ latitude: 37.8, longitude: -122.4, zoom: 10 }}
+        maxZoom={20}
+        minZoom={3}
+      >
+        <GeolocateControl position="top-left" />
+        <NavigationControl position="top-left" />
+
+        {freindspinData.map((pin, index) => {
+          return (
+            <div>
+              <Marker
+                key={index}
+                longitude={pin.Location.Longitude}
+                latitude={pin.Location.Latitude}
+              >
+                <button
+                  type="button"
+                  className="cursor-pointer"
+                  onClick={(e) => handleMarkerClick(pin)}
+                >
+                  {<MdLocationOn size={30} color="#0a3d62" />}
+                </button>
+              </Marker>
+
+              {showPopup ? (
+                <Popup
+                  offset={25}
+                  latitude={pin.Location.Latitude}
+                  longitude={pin.Location.Longitude}
+                  onClose={() => {
+                    setShowPopup(false);
+                  }}
+                  closeButton={false}
+                >
+                  <h3 className="">{pin.Name}</h3>
+                  <div className="">
+                    {/* <label className={classes.popupLabel}>Code: </label>
+							<span>{selectedMarker.airport.code}</span>
+							<br />
+							<label className={classes.popupLabel}>Country: </label>
+							<span>{selectedMarker.airport.country}</span>
+							<br />
+							<label className={classes.popupLabel}>Website: </label>
+							<Link
+								href={selectedMarker.airport.url === "" ? "#" : selectedMarker.airport.url}
+								target={selectedMarker.airport.url === "" ? null : "_blank"}
+								className={classes.popupWebUrl}
+							>
+								{selectedMarker.airport.url === "" ? "Nil" : selectedMarker.airport.url}
+							</Link> */}
+                  </div>
+                </Popup>
+              ) : null}
+            </div>
+          );
+        })}
+      </Map>
     </div>
-  )
+  );
 }
