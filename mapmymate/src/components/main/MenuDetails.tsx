037954da9da5f0d33831e9a0806748725bc51930
usmanashrf <45431758+usmanashrf@users.noreply.github.com>
2023-07-04 01:35:32 +0500

    make some changes in Menu Details

--- mapmymate/src/components/main/MenuDetails.tsx
+++ mapmymate/src/components/main/MenuDetails.tsx
@@ -1,8 +1,13 @@
 
 export default function MenuDetails() {
   return (
-    <div className="p-4 border-2 flex justify-center items-center flex-1">
- Details will be here..
+    <div className="relative p-4 border-2 flex flex-col justify-center items-center flex-1">
+      <div className="text-8xl flex flex-col justify-center items-center font-black text-[#0a3d62] opacity-10 absolute">
+        <div>MAP</div>
+        <div>MY</div>
+        <div>MATE</div>
+        </div>
+ <div>Details will be here..</div>
     </div>
   )
 }
