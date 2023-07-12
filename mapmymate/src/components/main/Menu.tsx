9e8e7521b998f73c088da01715c3dc5d68ddfb1a
usmanashrf <45431758+usmanashrf@users.noreply.github.com>
2023-07-13 02:53:38 +0500

    added resposive side menu with animation in main page

---
+++ mapmymate/src/components/main/Menu.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import SideMenuItems from './SideMenuItems'
+import MenuDetails from './MenuDetails'
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+  } from "@/components/ui/sheet"
+import { MenuIcon } from 'lucide-react'
+
+export default function Menu() {
+  return (
+    <Sheet>
+  <SheetTrigger><MenuIcon className='text-white'/></SheetTrigger>
+  <SheetContent>
+    <div className='bg-[#dff9fb] h-screen flex flex-col'>
+          
+            <SideMenuItems/>
+            <MenuDetails/>
+        </div>
+        </SheetContent>
+        </Sheet>
+  )
+}
--- mapmymate/src/components/main/index.tsx
+++ mapmymate/src/components/main/index.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import SideMenuItems from './SideMenuItems'
 import MenuDetails from './MenuDetails'
 import MateMap from '../map'
+import Menu from './Menu'
 
 export default function MainPage() {
   return (
-    <div className='flex'>
-        <div className='bg-gray-200 w-3/4 h-screen'>
+    <div className='flex relative'>
+        <div className='bg-gray-200 w-full h-screen'>
             <MateMap/>
         </div>
-        <div className='bg-[#dff9fb] w-1/4 h-screen flex flex-col'>
-            <SideMenuItems/>
-            <MenuDetails/>
+        <div className='absolute z-10 bg-[#0a3d62] px-4 py-2 rounded flex ml-auto right-2 top-2'>
+        <Menu/>
         </div>
     </div>
   )
